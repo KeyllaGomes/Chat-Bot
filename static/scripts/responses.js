d38167640010668512c9c8abbb4f243bc7350d33
Keylla Gomes <keyllagomesx@gmail.com>
2022-05-09 12:20:01 -0300

    ChatBot
    Inspirado em um vídeo

---
+++ static/scripts/responses.js
@@ -0,0 +1,22 @@
+function getBotResponse(input) {
+    if (input == "pedra") {
+        return "papel";
+    } else if (input == "papel") {
+
+    } else if (input == "papel") {
+        return "tesoura";
+    } else if(input == "tesoura") {
+        return "pedra";
+    }
+
+    if ( input == "ola") {
+        return "Ola!!";
+    } else if (input == "tchau") {
+        return "falo com você mais tarde";
+
+
+    } else {
+        return "Tente perguntar outra coisa"
+    }
+}
+
